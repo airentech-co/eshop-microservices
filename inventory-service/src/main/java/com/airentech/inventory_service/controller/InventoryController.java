90e1927b6221706326b2374737c8540259b38513
airentech-co <airentech.co@gmail.com>
2024-10-20 19:35:50 -0400

    initial commit - completed template skeleton

---
+++ inventory-service/src/main/java/com/airentech/inventory_service/controller/InventoryController.java
@@ -0,0 +1,20 @@
+package com.airentech.inventory_service.controller;
+
+import com.airentech.inventory_service.service.InventoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/inventory")
+public class InventoryController {
+
+    @Autowired
+    private InventoryService inventoryService;
+
+    @GetMapping("/{sku-code}")
+    @ResponseStatus(HttpStatus.OK)
+    public boolean isInStock(@PathVariable("sku-code") String skuCode) {
+        return inventoryService.isInStock(skuCode);
+    }
+}
---
+++ product-service/src/main/java/com/airentech/product_service/service/ProductService.java
@@ -0,0 +1,43 @@
+package com.airentech.product_service.service;
+
+import com.airentech.product_service.dto.ProductRequest;
+import com.airentech.product_service.dto.ProductResponse;
+import com.airentech.product_service.model.Product;
+import com.airentech.product_service.repository.ProductRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Slf4j
+public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    public void createProduct(ProductRequest productRequest) {
+        Product product = Product.builder()
+                .name(productRequest.getName())
+                .description(productRequest.getDescription())
+                .price(productRequest.getPrice())
+                .build();
+        productRepository.save(product);
+        log.info("Product " + product.getId() + " is created!");
+    }
+
+    public List<ProductResponse> getAllProducts() {
+        List<Product> products = productRepository.findAll();
+        return products.stream().map(this::mapToProductResponse).toList();
+    }
+
+    private ProductResponse mapToProductResponse(Product product) {
+        return ProductResponse.builder()
+                .id(product.getId())
+                .name(product.getName())
+                .description(product.getDescription())
+                .price(product.getPrice())
+                .build();
+    }
+}
