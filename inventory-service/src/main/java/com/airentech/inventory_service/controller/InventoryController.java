a4435d595f6bfb821b1940fcbe254494ed8792e0
airentech-co <airentech.co@gmail.com>
2024-10-20 21:17:08 -0400

    completed inter-service communication between OrderService and InventoryService by using WebFlux (asynchronous api)

--- inventory-service/src/main/java/com/airentech/inventory_service/controller/InventoryController.java
+++ inventory-service/src/main/java/com/airentech/inventory_service/controller/InventoryController.java
@@ -1,20 +1,36 @@
 package com.airentech.inventory_service.controller;
 
+import com.airentech.inventory_service.dto.InventoryResponse;
 import com.airentech.inventory_service.service.InventoryService;
+import jakarta.websocket.server.PathParam;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/inventory")
+@Slf4j
 public class InventoryController {
 
     @Autowired
     private InventoryService inventoryService;
 
-    @GetMapping("/{sku-code}")
+    // http://localhost:8003/api/inventory/iphone-12
+//    @GetMapping("/{sku-code}")
+//    @ResponseStatus(HttpStatus.OK)
+//    public boolean isInStock(@PathVariable("sku-code") String skuCode) {
+//        return inventoryService.isInStock(skuCode);
+//    }
+
+    // htpp://localhost:8003/api/inventory?sku-code=iphone-12&sku-code=iphone-12-red
+    @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    public boolean isInStock(@PathVariable("sku-code") String skuCode) {
+    public List<InventoryResponse> isInStock(@RequestParam List<String> skuCode) {
+        log.info("Received inventory check request for skuCode: {}", skuCode);
         return inventoryService.isInStock(skuCode);
     }
+
 }
--- order-serivce/src/main/java/com/airentech/order_service/service/OrderService.java
+++ order-serivce/src/main/java/com/airentech/order_service/service/OrderService.java
@@ -1,23 +1,29 @@
 package com.airentech.order_service.service;
 
+import com.airentech.order_service.dto.InventoryResponse;
 import com.airentech.order_service.dto.OrderLineItemsDto;
 import com.airentech.order_service.dto.OrderRequest;
 import com.airentech.order_service.model.Order;
 import com.airentech.order_service.model.OrderLineItems;
 import com.airentech.order_service.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 @Transactional
+@Slf4j
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final WebClient webClient;
 
     public void placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
@@ -27,7 +33,23 @@ public class OrderService {
 
         order.setOrderLineItemsList(orderLineItems);
 
-        orderRepository.save(order);
+        List<String> skuCodes = order.getOrderLineItemsList().stream().map(OrderLineItems::getSkuCode).toList();
+
+        // if all skuCodes are in the stock
+        InventoryResponse[] inventoryResponseArray = webClient.get()
+                .uri("http://localhost:8003/api/inventory", uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
+                .retrieve()
+                .bodyToMono(InventoryResponse[].class)
+                .block();
+
+        assert inventoryResponseArray != null;
+
+        log.info("response from inventory service: {}", Arrays.toString(inventoryResponseArray));
+
+        boolean allProductsInStock = Arrays.stream(inventoryResponseArray).allMatch(InventoryResponse::isInStock);
+
+        if (allProductsInStock) orderRepository.save(order);
+        else throw new IllegalArgumentException("Product is not in stock, please try again.");
     }
 
     private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto) {
